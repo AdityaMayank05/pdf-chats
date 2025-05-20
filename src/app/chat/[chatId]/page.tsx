f27e75a079230a1f553168a25ba2a95f68bd5174
AdityaMayank05 <108828982+AdityaMayank05@users.noreply.github.com>
2025-05-21 01:12:28 +0530

    err

--- src/app/chat/[chatId]/page.tsx
+++ src/app/chat/[chatId]/page.tsx
@@ -16,7 +16,7 @@ type Props = {
 };
 
 const ChatPage = async ({ params}: Props) => {
-  const { chatId } = await params;
+  const { chatId } = params;
   const { userId } = await auth();
   if (!userId) {
     return redirect("/sign-in");
